174a9c2d7dcf00a9fcacb9283d24c807e9f0aa62
syinyang <ba76191777.163.com>
2017-06-11 12:58:42 +0800

    完成讲师查看功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,12 +1,30 @@
-define(['jquery','template'],function($,template) {
+define(['jquery','template','bootstrap'],function($,template) {
 	$.ajax({
 		dataType: 'json',
 		url: '/api/teacher',
 		success: function(data) {
-			console.log(data);
+			// console.log(data);
 			if(data.code == 200) {
 				var html = template('tealistTpl',{list: data.result});
 				$(".table tbody").html(html);
+
+				$("#teacherInfo").find(".preview").click(function() {
+					var tcId = $(this).closest('td').attr('data-id');
+					$.ajax({
+						url: '/api/teacher/view',
+						dataType: 'json',
+						data: {
+							tc_id: tcId
+						},
+						success: function(data) {
+							data.result.tc_hometown = data.result.tc_hometown.replace(/\|/g,' ');
+							var html = template('teacherModalInfoTpl',data.result);
+							$("#teacherModalInfo").html(html);
+							$("#teacherModal").modal();
+						}
+					});
+					return false;
+				});
 			}
 		}
 	});
