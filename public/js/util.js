e5ff8a2874848c7f6303b2eef9a59f52f409f5ac
syinyang <ba76191777.163.com>
2017-06-11 20:15:19 +0800

    实现根据id查询值的功能

--- public/js/util.js
+++ public/js/util.js
@@ -3,8 +3,20 @@ define(['jquery'],function($) {
 		setMenu: function(pathname) {
 			$(".aside .navs a").removeClass('active');
 			$(".aside .navs a[href='"+ pathname +"']").addClass('active');
-		}
-			
+		},
+		qs: function(attr,param) {
+			param = param.substring(1);
+			var arr = param.split('&');
+			var retValue = null;
+			arr.forEach(function(e,i) {
+				var kv = e.split('=');
+				if(attr == kv[0]) {
+					retValue = kv[1];
+					return;
+				} 
+			});
+			return retValue;
+		} 	
 		
 	}
 	
