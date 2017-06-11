3472c1bfcdc0ae0ac477eb57448f781305850e42
syinyang <ba76191777.163.com>
2017-06-11 14:14:03 +0800

    实现讲师注销开启功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -8,24 +8,56 @@ define(['jquery','template','bootstrap'],function($,template) {
 				var html = template('tealistTpl',{list: data.result});
 				$(".table tbody").html(html);
 
-				$("#teacherInfo").find(".preview").click(function() {
-					var tcId = $(this).closest('td').attr('data-id');
-					$.ajax({
-						url: '/api/teacher/view',
-						dataType: 'json',
-						data: {
-							tc_id: tcId
-						},
-						success: function(data) {
-							data.result.tc_hometown = data.result.tc_hometown.replace(/\|/g,' ');
-							var html = template('teacherModalInfoTpl',data.result);
-							$("#teacherModalInfo").html(html);
-							$("#teacherModal").modal();
-						}
-					});
-					return false;
-				});
+				previewTeacher();
+				enableOrDisableTeacher();
 			}
 		}
 	});
+
+	function previewTeacher() {
+		$("#teacherInfo").find(".preview").click(function() {
+			var tcId = $(this).closest('td').attr('data-id');
+			$.ajax({
+				url: '/api/teacher/view',
+				dataType: 'json',
+				data: {
+					tc_id: tcId
+				},
+				success: function(data) {
+					data.result.tc_hometown = data.result.tc_hometown.replace(/\|/g,' ');
+					var html = template('teacherModalInfoTpl',data.result);
+					$("#teacherModalInfo").html(html);
+					$("#teacherModal").modal();
+				}
+			});
+			return false;
+		});
+	}
+	function enableOrDisableTeacher() {
+		$("#teacherInfo").find(".edteacher").click(function() {
+			var that = this;
+			var td = $(this).closest('td');
+			var tcId = td.attr('data-id');
+			var tcStatus = td.attr('data-status');
+			$.ajax({
+				type: 'post',
+				url: '/api/teacher/handle',
+				dataType: 'json',
+				data: {
+					tc_id: tcId,
+					tc_status: tcStatus
+				},
+				success: function(data) {
+					if(data.code == 200) {
+						td.attr('data-status',data.result.tc_status);
+						if(data.result.tc_status == 0) {
+							$(that).html("注 销");
+						}else {
+							$(that).html("开 启");
+						}
+					}
+				}
+			});
+		});
+	}
 });
